98b5f9eb3fd01dc057fd2617195526e66df509ef
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-09-15 22:11:02 +0200

    justify coments

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Backend server is running!')
-})
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5500
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
